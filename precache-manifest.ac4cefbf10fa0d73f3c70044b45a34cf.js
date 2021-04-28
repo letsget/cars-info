b239aebd341ef7117f50a690986422e0c2868971
Andrey Bondarev <andrewbond25@gmail.com>
2021-04-28 11:45:05 +0200

    Updates

---
+++ precache-manifest.ac4cefbf10fa0d73f3c70044b45a34cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b85787a3db501647813dc2692ccaae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9cb89d952b4f291fa61",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "ad1dc713e84b26c2f7d4",
+    "url": "/static/css/main.4c707c9f.chunk.css"
+  },
+  {
+    "revision": "c9cb89d952b4f291fa61",
+    "url": "/static/js/2.7e4225ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7e4225ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad1dc713e84b26c2f7d4",
+    "url": "/static/js/main.dae39a58.chunk.js"
+  },
+  {
+    "revision": "036ab1b84fdcb7db0579",
+    "url": "/static/js/runtime-main.b91b3eea.js"
+  }
+]);
